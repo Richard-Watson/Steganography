efd058f285dfef8ef8935320d09b3660127c6399
Richard-Watson <vladislav.lisyanskiy@gmail.com>
2016-02-01 14:35:00 +0600

    Сделал окно кодирования по-умолчанию при открытии

--- UI.py
+++ UI.py
@@ -83,7 +83,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.MainTab.setCurrentIndex(1)
+        self.MainTab.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
