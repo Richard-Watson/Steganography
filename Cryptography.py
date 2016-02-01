9cc6bdf2beee447e26a375c000d6b03a1d18a975
Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>
2016-02-01 08:39:05 +0600

    Изменил функцию шифрования на XOR

--- Cryptography.py
+++ Cryptography.py
@@ -1,27 +1,5 @@
-import base64
-from cryptography.fernet import Fernet, InvalidToken
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-def cryptSHA(bytestring, password, mode = True):
-    password = bytes(password, encoding="UTF-8")
-    salt = b'l,4B<\x0e\xff#\xc2\xe8\xe59\xbe\xdf8C'
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-        backend=default_backend()
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(password))
-    f = Fernet(key)
-
-    if mode:
-        token = f.encrypt(bytestring)
-        return token
-    else:
-        try:
-            return f.decrypt(bytestring)
-        except InvalidToken:
-            pass
+def cryptXOR(Input, password):
+    Output = ""
+    for i in range(len(Input)):
+        Output += chr(Input[i] ^ ord(password[i % len(password)]))
+    return bytes(Output, encoding="iso8859-1")
